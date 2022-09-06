7df6604303495f219ad1c004fd00206a353af8e7
윤관 <blackgar@naver.com>
2022-09-06 18:21:47 +0900

    Chore: 프로젝트 초기 세팅(스타일, 컴포넌트 구조, 패키지, 라우터 설정)

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from 'styled-components';
+import Router from './Routes';
+import GlobalStyle from './styles/GlobalStyle';
+import { lightTheme } from './styles/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+function App() {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
