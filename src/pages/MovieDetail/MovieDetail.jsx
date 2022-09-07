aa5f029e8d7335c19b18bdd91ed52a223141eb04
Dev-jwJeong <rgn95652@gmail.com>
2022-09-07 15:42:20 +0900

    Feat : 영화 상세페이지 컴포넌트 flex로 변경

--- src/pages/MovieDetail/MovieDetail.jsx
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import MovieCast from './MovieCast/MovieCast';
+import MovieExtraDetail from './MovieExtraDetail/MovieExtraDetail';
 import MovieOpinion from './MovieOpinion/MovieOpinion';
+import MovieSuggest from './MovieSuggest/MovieSuggest';
 
 function MovieDetail() {
   return (
@@ -9,15 +11,32 @@ function MovieDetail() {
       <MovieDetailLower>
         <MovieCast />
         <MovieOpinion />
+        <MovieSuggest />
       </MovieDetailLower>
+      <MovieDetailAside>
+        <MovieExtraDetail />
+      </MovieDetailAside>
     </MovieDetailBlock>
   );
 }
 
 export default MovieDetail;
 
-const MovieDetailBlock = styled.div``;
+const MovieDetailBlock = styled.div`
+  width: 1440px;
+  padding: 1rem 5rem;
+  margin: 0 auto;
+  display: flex;
+
+  @media (max-width: 1440px) {
+    width: 100%;
+  }
+`;
+const MovieDetailAside = styled.div`
+  width: 30%;
+  padding-top: 1rem;
+`;
 const MovieDetailLower = styled.div`
-  padding: 1rem;
   width: 70%;
+  margin-right: 3rem;
 `;
