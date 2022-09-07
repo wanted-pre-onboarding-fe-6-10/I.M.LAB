59129259e6ace73deb90a2b3f8bea3c319511cec
qkrwlstjd <qkrwls91877276@gmail.com>
2022-09-07 17:03:55 +0900

    feat: 네비게이션, 검색어 자동완성 구현

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Nav from './components/Nav';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
