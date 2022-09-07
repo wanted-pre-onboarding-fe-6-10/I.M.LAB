bb8eabbd821d820b22e9f28e8bfc7229a854addc
Dev-jwJeong <rgn95652@gmail.com>
2022-09-07 13:49:13 +0900

    Feat : 모든 리뷰 보기 컴포넌트

---
+++ src/pages/MovieDetail/MovieReview/MovieReview.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+import { lightTheme } from '../../../styles/theme';
+import MovieReviewModal from '../MovieOpinion/MovieReviewModal';
+
+function MovieReview() {
+  return (
+    <MovieReviewBlock>
+      <MovieInfo>11</MovieInfo>
+      <MovieReviewBox>
+        <ReviewButton>리뷰 쓰기</ReviewButton>
+        <MovieReviewModal />
+      </MovieReviewBox>
+    </MovieReviewBlock>
+  );
+}
+
+export default MovieReview;
+
+const MovieReviewBlock = styled.div`
+  width: 100%;
+`;
+const MovieInfo = styled.div`
+  width: 100%;
+  height: 100px;
+  background-color: black;
+`;
+
+const MovieReviewBox = styled.div`
+  width: 70%;
+  margin: 1rem auto;
+`;
+const ReviewButton = styled.button`
+  margin-right: 1rem;
+  display: flex;
+  align-items: center;
+  background-color: ${lightTheme.textColor};
+  border: none;
+  outline: none;
+  color: ${lightTheme.bgColor};
+  border-radius: 20px;
+  padding: 0.5rem 1.5rem;
+  margin-bottom: 1rem;
+  font-weight: bold;
+  cursor: pointer;
+`;
