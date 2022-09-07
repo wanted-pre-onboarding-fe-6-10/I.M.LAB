11713000a7ff3b085f42c5ab5f83b3c1fda2dc1f
eazae <xynezae@gmail.com>
2022-09-07 17:15:23 +0900

    Fix: initial execution configurations - pre-commit, pre-push auth - App.jsx wrong import - Router.jsx default export - api.jsx wrong constant naming

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,5 +1,5 @@
-const BASE_URL = `${process.env.BASE_URL}`;
-const API_KEY = `${process.env.API_KEY}`;
+const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 
 export async function fetchPopularMovie(page) {
   return fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=ko-KR&page=${page}`).then(
