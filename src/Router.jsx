c0969df0aa78d9dd1b46175d8a2ffc7b3e447289
Dev-jwJeong <rgn95652@gmail.com>
2022-09-07 13:46:48 +0900

    feat : 영화상세페이지, 모든 리뷰보기 url 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+import MovieReview from './pages/MovieDetail/MovieReview/MovieReview';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:movieId/:movieName" element={<MovieDetail />} />
+        <Route path="/:movieId/:movieName/reviews" element={<MovieReview />} />
       </Routes>
     </BrowserRouter>
   );
 };
+
+export default Router;
