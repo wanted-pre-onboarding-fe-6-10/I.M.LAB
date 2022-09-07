868564bd177c97baf07a2cc386192be06528040c
[SW2/서울/2반]김한얼 <gksdjf80@naver.com>
2022-09-07 17:08:28 +0900

    Refactor : api 수정사항 반영

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,5 +1,5 @@
-const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
-const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export async function fetchPopularMovie(page) {
   return await fetch(
