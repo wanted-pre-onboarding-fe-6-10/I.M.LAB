2ff39c455d3f90ec2f742ef34271f8240fc93c13
eazae <xynezae@gmail.com>
2022-09-08 00:32:29 +0900

    Feat: card component for movies tab list

--- src/pages/Home/MoviesTab/NowPlayingList.jsx
+++ src/pages/Home/MoviesTab/NowPlayingList.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import styled from 'styled-components';
 import { fetchNowPlayingMovie } from '../../../api/api';
-import { IMG_BASE_URL, PreviewCard } from './Temp';
+import MoviesCard from './MoviesCard';
 
 const NowPlayingList = () => {
   const { isLoading, data, refetch } = useQuery(['homeNowPlaying'], () => fetchNowPlayingMovie(1), {
@@ -11,15 +11,7 @@ const NowPlayingList = () => {
   // 한시간마다 갱신되게 처리
   return (
     <Wrapper>
-      {!isLoading
-        ? data.results.map(item => (
-            <PreviewCard key={item.id}>
-              {item.id}
-              <img src={IMG_BASE_URL + item.poster_path} alt={item.title} />
-              {item.title}
-            </PreviewCard>
-          ))
-        : '로딩중 '}
+      {!isLoading ? data.results.map(item => <MoviesCard key={item.id} item={item} />) : '로딩중 '}
     </Wrapper>
   );
 };
