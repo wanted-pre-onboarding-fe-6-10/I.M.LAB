29434a5b2a423d6536d5d13915327f7eed61489c
윤관 <blackgar@naver.com>
2022-09-08 00:52:55 +0900

    feat: sort 기능 개발 완료

--- src/pages/TopRated/TopRated.jsx
+++ src/pages/TopRated/TopRated.jsx
@@ -1,13 +1,21 @@
 import { useQuery, useInfiniteQuery } from '@tanstack/react-query';
+import { rearg } from 'lodash';
 import React, { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { fetchNowPlayingMovie, fetchTopRatedMovie, fetchUpcomingMovie } from '../../api/api';
+import { genres } from '../../utils/constant';
+import { sortBy } from '../../utils/sort';
 
 const TopRated = () => {
   const { ref, inView } = useInView();
   const [topRatedMovies, setTopRatedMovies] = useState([]);
   const [total, setTotal] = useState(0);
-  const { status, isFetching, data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+  const {
+    status,
+    data: topRatedMovieList,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery(
     ['infinite', 'top_rated'],
     ({ pageParam = 1 }) => fetchTopRatedMovie(pageParam),
     {
@@ -16,14 +24,17 @@ const TopRated = () => {
       },
     }
   );
+
   const getTMDB = async page => {
     const res = await fetchTopRatedMovie(page);
-    if (res.total_pages > 500) {
-      setTotal(500);
-    } else {
-      setTotal(res.total_pages);
+    if (!total) {
+      if (res.total_pages > 500) {
+        setTotal(500);
+      } else {
+        setTotal(res.total_pages);
+      }
     }
-    setTopRatedMovies(prev => [...prev, res.results]);
+    res.results.forEach(v => setTopRatedMovies(prev => [...prev, v]));
   };
 
   useEffect(() => {
@@ -36,6 +47,14 @@ const TopRated = () => {
       for (let i = 1; i < total + 1; i++) {
         getTMDB(i);
       }
+      // *************************************************************
+      // *************************************************************
+      // 해당 부분은 필터 부분의 dropbox를 선택할 때 동작하도록 구현하면 됨
+      // dropbox별로 키값을 value로 정하고 선택하면 해당 키값으로 동작하게끔 구현하기.
+      // *************************************************************
+      // *************************************************************
+      // const sortResult = sortBy(topRatedMovies, 'vote_average_asc');
+      // console.log(sortResult);
     }
   }, [total]);
 
@@ -49,14 +68,13 @@ const TopRated = () => {
       fetchNextPage();
     }
   }, [inView]);
-
   return (
     <>
       {status === 'loading' ? (
         <>Loading...</>
       ) : (
         <div>
-          {data.pages.map((page, i) => (
+          {topRatedMovieList.pages.map((page, i) => (
             <React.Fragment key={i}>
               {page.results.map(v => (
                 <div key={v.id}>
