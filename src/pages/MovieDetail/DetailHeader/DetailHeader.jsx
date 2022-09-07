0694a43be72a262f338ead1daaf0a03339a8ba8f
jihyun-jeon <jhplus13@naver.com>
2022-09-07 11:28:41 +0900

    Feat : 구매,대여 아이콘에 주소 연결

--- src/pages/MovieDetail/DetailHeader/DetailHeader.jsx
+++ src/pages/MovieDetail/DetailHeader/DetailHeader.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { fetchMovieDetail, fetchMovieVides, fetchMovieBuy } from '../../../api/api';
 // https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>&language=en-US
 
@@ -6,15 +7,18 @@ const DetailHeader = () => {
   const [detailData, setDetailData] = useState({});
   const [videosData, setVideosData] = useState({});
   const [buyData, setBuyData] = useState({});
-
-  console.log(buyData);
+  const getRequest = async () => {
+    await fetchMovieVides(557).then(result => setVideosData(result));
+    await fetchMovieBuy(557).then(result => setBuyData(result.results.KR));
+    await fetchMovieDetail(557).then(result => setDetailData(result));
+  };
 
   useEffect(() => {
     // [질문] api호출 여러개 있는데 이렇게 하는거 맞는지?
-    fetchMovieVides(557).then(result => setVideosData(result));
-    fetchMovieBuy(557).then(result => setBuyData(result.results.KR));
-    fetchMovieDetail(557).then(result => setDetailData(result));
+    getRequest();
   }, []);
+
+  console.log(buyData);
 
   return (
     <div>
@@ -25,7 +29,7 @@ const DetailHeader = () => {
             alt="img"
             width="100%"
             height="100%"
-            style={{ opacity: 0.1 }}
+            style={{ opacity: 0.3 }}
             src={`https://image.tmdb.org/t/p/original${detailData?.backdrop_path}`}
           />
 
@@ -37,7 +41,8 @@ const DetailHeader = () => {
               height: '100%',
               top: 0,
               display: 'grid',
-              gridTemplateColumns: '60% 40%',
+              placeItems: 'center',
+              gridTemplateColumns: '50% 50%',
             }}
           >
             <iframe
@@ -70,37 +75,65 @@ const DetailHeader = () => {
                   <span key={data.id}>{data.name}</span>
                 ))}
               </p>
-              <p style={{ display: 'flex' }}>
+              <div style={{ display: 'flex' }}>
                 rate : {Math.floor(detailData.vote_average)}
                 <ul style={{ display: 'flex' }}>
                   <li>좋아요</li>
                   <li>북마크</li>
                   <li>별표(클릭시 평점별5개)</li>
                 </ul>
-              </p>
+              </div>
               <p>{detailData.tagline}</p>
               <p>{detailData.overview}</p>
               <div>
-                buy
-                {buyData.buy.map((data, idx) => (
-                  <img
-                    key={idx}
-                    src={`https://image.tmdb.org/t/p/original${data.logo_path}`}
-                    width="50"
-                    height="50"
-                    alt="img"
-                  />
-                ))}
-                rent
-                {buyData.rent.map((data, idx) => (
-                  <img
-                    key={idx}
-                    src={`https://image.tmdb.org/t/p/original${data.logo_path}`}
-                    width="50"
-                    height="50"
-                    alt="img"
-                  />
-                ))}
+                <div>
+                  buy
+                  {buyData.buy?.map((data, idx) => (
+                    <a href={`${BUY_URL[data.provider_name]}`} key={idx}>
+                      <img
+                        src={`https://image.tmdb.org/t/p/original${data.logo_path}`}
+                        width="50"
+                        height="50"
+                        alt="img"
+                      />
+                    </a>
+                  ))}
+                  {buyData.flatrate?.map((data, idx) => (
+                    <a href={`${BUY_URL[data.provider_name]}`} key={idx}>
+                      <img
+                        src={`https://image.tmdb.org/t/p/original${data.logo_path}`}
+                        width="50"
+                        height="50"
+                        alt="img"
+                      />
+                    </a>
+                  ))}
+                </div>
+                <div>
+                  rent
+                  {buyData.rent?.map((data, idx) => (
+                    <a href={`${BUY_URL[data.provider_name]}`} key={idx}>
+                      <img
+                        key={idx}
+                        src={`https://image.tmdb.org/t/p/original${data.logo_path}`}
+                        width="50"
+                        height="50"
+                        alt="img"
+                      />
+                    </a>
+                  ))}
+                </div>
+                <div>
+                  {buyData.free?.map((data, idx) => (
+                    <img
+                      key={idx}
+                      src={`https://image.tmdb.org/t/p/original${data.logo_path}`}
+                      width="50"
+                      height="50"
+                      alt="img"
+                    />
+                  ))}
+                </div>
               </div>
             </div>
           </div>
@@ -111,3 +144,21 @@ const DetailHeader = () => {
 };
 
 export default DetailHeader;
+
+const BUY_URL = {
+  Netflix: 'https://www.netflix.com/kr/',
+  wavve:
+    'https://www.wavve.com/?utm_source=google&utm_medium=search&utm_campaign=wavve_pc&utm_content=0.%EB%B8%8C%EB%9E%9C%EB%93%9C%ED%82%A4%EC%9B%8C%EB%93%9C&utm_term=WAVVE&gclid=Cj0KCQjw39uYBhCLARIsAD_SzMQ5LNlYwfOHqW-QQ1i0lD2dAbwBGmgcfm2rQsitgnB7VpiFP-5kqsUaAjo-EALw_wcB',
+  'Google Play Movies': 'https://play.google.com/store/movies',
+  'Naver Store': 'https://serieson.naver.com/v2/movie',
+  'Disney Plus':
+    'https://www.disneyplus.com/ko-kr?cid=DSS-Search-Google-71700000088339394-&s_kwcid=AL!8468!3!584589737656!b!!g!!%EB%94%94%EC%A6%88%EB%8B%88%20%ED%94%8C%EB%9F%AC%EC%8A%A4&gclid=Cj0KCQjw39uYBhCLARIsAD_SzMSEzHnOztm1jAwuZHJxDXmnL6ee4giIsV8s69uMZZnLTJPdEQvX4t8aAkFwEALw_wcB&gclsrc=aw.ds',
+  Plex: 'https://www.plex.tv/',
+  'Amazon Prime Video': 'https://www.primevideo.com/ref=atv_dp_mv_c_9zZ8D2_1_0',
+  'Amazon Video': 'https://www.amazon.com/Amazon-Video/b?ie=UTF8&node=2858778011',
+  'Apple iTunes': 'https://itunes.apple.com/us/genre/movies/id33',
+  'Claro video': 'https://www.clarovideo.com/',
+  'Sky Store': 'https://www.skystore.com/',
+  'Microsoft Store': 'https://www.microsoft.com/en-us/store/movies-and-tv',
+  YouTube: 'https://www.youtube.com/feed/storefront',
+};
