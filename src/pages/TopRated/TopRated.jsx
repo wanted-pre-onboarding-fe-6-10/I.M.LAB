5af8be02ddafd02e85dddee031a6bc63a2f0f34a
윤관 <blackgar@naver.com>
2022-09-07 19:49:22 +0900

    feat: sort와 filter를 위한 데이터 api요청

--- src/pages/TopRated/TopRated.jsx
+++ src/pages/TopRated/TopRated.jsx
@@ -1,11 +1,13 @@
 import { useQuery, useInfiniteQuery } from '@tanstack/react-query';
 import React, { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
-import { fetchNowPlayingMovie, fetchTopRatedMovie } from '../../api/api';
+import { fetchNowPlayingMovie, fetchTopRatedMovie, fetchUpcomingMovie } from '../../api/api';
 
 const TopRated = () => {
   const { ref, inView } = useInView();
-  const { status, data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [total, setTotal] = useState(0);
+  const { status, isFetching, data, fetchNextPage, hasNextPage } = useInfiniteQuery(
     ['infinite', 'top_rated'],
     ({ pageParam = 1 }) => fetchTopRatedMovie(pageParam),
     {
@@ -14,7 +16,31 @@ const TopRated = () => {
       },
     }
   );
+  const getTMDB = async page => {
+    const res = await fetchTopRatedMovie(page);
+    if (res.total_pages > 500) {
+      setTotal(500);
+    } else {
+      setTotal(res.total_pages);
+    }
+    setTopRatedMovies(prev => [...prev, res.results]);
+  };
+
   useEffect(() => {
+    getTMDB();
+  }, []);
+
+  useEffect(() => {
+    if (total) {
+      setTopRatedMovies([]);
+      for (let i = 1; i < total + 1; i++) {
+        getTMDB(i);
+      }
+    }
+  }, [total]);
+
+  useEffect(() => {
+    if (status === 'loading') return;
     if (!hasNextPage) {
       alert('마지막 페이지입니다.');
       return;
@@ -23,6 +49,7 @@ const TopRated = () => {
       fetchNextPage();
     }
   }, [inView]);
+
   return (
     <>
       {status === 'loading' ? (
