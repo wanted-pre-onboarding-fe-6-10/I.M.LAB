41279a909dc6616a4408d817b835ea488770799c
윤관 <blackgar@naver.com>
2022-09-07 16:12:12 +0900

    feat: useInfiniteQuery를 활용한 infinite scroll 적용

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import TopRated from './pages/TopRated/TopRated';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/top_rated" element={<TopRated />} />
       </Routes>
     </BrowserRouter>
   );
